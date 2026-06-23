9156e29c55c0a2bcb541501477bbc1f2bdc00841
Akashkeni <keniakash07@gmail.com>
2026-06-23 18:57:49 +0530

    Added Main Hero Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import { StarBackground } from "@/components/StarBackground"
 import ThemeToggle from "@/components/ThemeToggle"
 import { Navbar } from "@/components/Navbar"
+import { HeroSection } from "@/components/HeroSection"
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-
+    
     <ThemeToggle/>
 
     <StarBackground/>
 
     <Navbar/>
+
+    <main>
+      <HeroSection/>
+    </main>
+
     </div>
   )
 }
